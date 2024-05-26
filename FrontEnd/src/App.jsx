5761cbe41ebe1f1fdc29fcdacc4aa7da8e0bc1ce
JosefMontanari <josef.montanari94@gmail.com>
2024-05-26 12:07:06 +0200

    Categories and footer

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,16 +4,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/placeorder" element={<PlaceOrder />}></Route>
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/placeorder" element={<PlaceOrder />}></Route>
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
